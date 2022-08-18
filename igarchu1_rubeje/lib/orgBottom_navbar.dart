1101be3b58bbaf3432d0e1efef4d60d7febe410a
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-08-19 05:56:37 +0800

    Design Pet Adoption ver1

---
+++ igarchu1_rubeje/lib/orgBottom_navbar.dart
@@ -0,0 +1,78 @@
+// // ignore: camel_case_types
+// import 'package:flutter/material.dart';
+// import 'package:igarchu1_rubeje/screens/adoption/adoption_screen.dart';
+
+// import '../../constants.dart';
+// import 'controllers/auth_controller.dart';
+
+// class OrgBottomNavBar extends StatefulWidget {
+//   const OrgBottomNavBar({Key? key}) : super(key: key);
+
+//   @override
+//   State<OrgBottomNavBar> createState() => _OrgBottomNavBarState();
+// }
+
+// // ignore: camel_case_types
+// class _OrgBottomNavBarState extends State<OrgBottomNavBar> {
+//   final AuthController _authController = AuthController();
+//   int currentIndex = 2;
+//   final screens = [
+//     AdoptionScreen(_authController)
+//    //screens
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: IndexedStack(
+//         index: currentIndex,
+//         children: (screens),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: currentIndex,
+//         type: BottomNavigationBarType.fixed,
+//         unselectedItemColor: Colors.grey,
+//         selectedItemColor: kbutton1,
+//         backgroundColor: Colors.white,
+//         iconSize: 30,
+//         onTap: (index) => setState(() {
+//           currentIndex = index;
+//         }),
+//         items: const [
+//           BottomNavigationBarItem(
+//             label: 'Health',
+//             icon: Icon(
+//               Icons.monitor_heart_outlined,
+//             ),
+//           ),
+//           BottomNavigationBarItem(
+//             label: 'Donate',
+//             icon: Icon(
+//               Icons.volunteer_activism_outlined,
+//             ),
+//           ),
+//           BottomNavigationBarItem(
+//             label: 'Explore',
+//             icon: Icon(Icons.search_outlined),
+//           ),
+//           BottomNavigationBarItem(
+//             label: 'Chat',
+//             icon: Icon(
+//               Icons.chat_outlined,
+//             ),
+//           ),
+//           BottomNavigationBarItem(
+//             label: 'Profile',
+//             icon: Icon(
+//               Icons.person_outline_outlined,
+//             ),
+//           ),
+//           // BottomNavigationBarItem(
+//           //   label: 'Settings',
+//           //   icon: Icon(Icons.settings),
+//           // ),
+//         ],
+//       ),
+//     );
+//   }
+// }
